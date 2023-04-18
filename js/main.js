005e20c71726dc8c8a5173d43b82ebce39da9817
DoBaoTrung <haiteenhp1@gmail.com>
2023-04-18 10:09:17 +0700

    upload source code

--- js/main.js
+++ js/main.js
@@ -45,7 +45,7 @@ function check() {
 
     if ((loginid.value != 0  && password.value == storedPassword) || (loginid.value == storeTel && password.value == storedPassword)) {
         alert('Đăng nhập thành công');
-        window.location = "https://dobaotrung.github.io/TemplateDemo/homeLogined.html";
+        window.location.href = "homeLogined.html";
     } else if (loginid.value.length == 0 && password.value.length == 0) {
         alert('Vui lòng nhập đầy đủ');
     } else if (loginid.value.length == 0) {
